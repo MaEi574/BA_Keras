8533952963497313d24b1849d30e22d073e241c7
Maximilian Eisenach <maximilian.eisenach@st.oth-regensburg.de>
2026-09-02 03:35:04 +0200

    add remote files

---
+++ Remote_Files/tcl_scripts/Analyze_Build_Verilog.tcl
@@ -0,0 +1,77 @@
+read_verilog [glob ./SampleNeuralNet_prj/solution1/syn/verilog/*.v]
+
+# ============================================================
+# Synthesis
+# ============================================================
+
+synth_design \
+    -top SampleNeuralNet \
+    -part xc7z010clg400-1 \
+    -mode out_of_context
+
+create_clock -name ap_clk -period 4.000 [get_ports ap_clk]
+
+file mkdir ./Analysis_Verilog
+
+# ============================================================
+# Post-Synthesis Analysis
+# ============================================================
+
+report_timing_summary -file ./Analysis_Verilog/Timing_Report_Synthesis.rpt
+report_timing -max_paths 20 -file ./Analysis_Verilog/Timing_Paths_Synthesis.rpt
+
+report_utilization -hierarchical -file ./Analysis_Verilog/Utilization_Report_Synthesis.rpt
+
+# ============================================================
+# DSP48E1 Inspection
+# ============================================================
+
+set dsps [get_cells -hierarchical -filter {REF_NAME == DSP48E1}]
+
+set fh [open "./Analysis_Verilog/DSP_Properties.rpt" w]
+
+puts $fh "DSP48E1 count: [llength $dsps]"
+puts $fh ""
+
+foreach dsp $dsps {
+    puts $fh "--------------------------------------------------"
+    puts $fh "DSP: $dsp"
+
+    foreach prop {
+        AREG
+        BREG
+        MREG
+        PREG
+        CREG
+        OPMODEREG
+        ALUMODEREG
+        USE_MULT
+        PRIMITIVE_TYPE
+    } {
+        puts $fh "$prop = [get_property $prop $dsp]"
+    }
+
+    puts $fh ""
+}
+
+close $fh
+
+write_checkpoint -force ./Analysis_Verilog/Post_Synthesis.dcp
+
+# ============================================================
+# Implementation
+# ============================================================
+
+opt_design
+place_design
+route_design
+
+# ============================================================
+# Post-Implementation Analysis
+# ============================================================
+
+report_timing_summary -file ./Analysis_Verilog/Timing_Report_Implementation.rpt
+report_timing -max_paths 20 -file ./Analysis_Verilog/Timing_Paths_Implementation.rpt
+report_utilization -hierarchical -file ./Analysis_Verilog/Utilization_Report_Implementation.rpt
+
+write_checkpoint -force ./Analysis_Verilog/Post_Implementation.dcp
